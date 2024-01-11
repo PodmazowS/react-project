8a11fc2b89df732e244595ff134c895068c65ba1
PodmazowS <podmazowstas@gmail.com>
2024-01-12 00:23:09 +0100

    Add header and footer components to MainComponent

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,20 @@ import Header from './components/common/Header/header';
 import Home from './pages/home/Home';
 import UserProfilePage from './pages/UserProfilePage/UserProfile';
 import PageNotFound from './pages/pagenotfound/PageNotFound';
+import Footer from './components/common/Footer/footer';
 
 
 const MainComponent = () => {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />}  />
         <Route path="/user/:userID" element={<UserProfilePage />} /> 
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
